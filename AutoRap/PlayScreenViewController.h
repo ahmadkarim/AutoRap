656df35579033559f0b61407543907ab13f2b3ba
ahmadkarim <ahmadkarim@Ahmads-MacBook-Pro.local>
2014-07-01 00:05:48 +0500

    new view controllers made and hooked up with TAAE

---
+++ AutoRap/PlayScreenViewController.h
@@ -0,0 +1,24 @@
+//
+//  PlayScreenViewController.h
+//  AutoRap
+//
+//  Created by Ahmad Karim on 30/06/2014.
+//  Copyright (c) 2014 Ahmad Karim. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "TheAmazingAudioEngine.h"
+#import "AEPlaythroughChannel.h"
+#import "AEExpanderFilter.h"
+#import "AELimiterFilter.h"
+#import "AERecorder.h"
+#import <QuartzCore/QuartzCore.h>
+#import <AEAudioController.h>
+
+@interface PlayScreenViewController : UIViewController{
+   
+    AEChannelGroupRef _group;
+}
+
+@end
--- AutoRap/PlayScreenViewController.m
+++ AutoRap/PlayScreenViewController.m
@@ -1,19 +1,14 @@
 //
-//  ViewController.m
+//  PlayScreenViewController.m
 //  AutoRap
 //
-//  Created by Ahmad Karim on 26/06/2014.
+//  Created by Ahmad Karim on 30/06/2014.
 //  Copyright (c) 2014 Ahmad Karim. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "PlayScreenViewController.h"
 
-
-
-
-
-@interface ViewController ()
-
+@interface PlayScreenViewController ()
 
 @property (nonatomic, strong) AEAudioController *audioController;
 @property (nonatomic, strong) AEAudioFilePlayer *loop1;
@@ -21,20 +16,23 @@
 @property (nonatomic, strong) AERecorder *recorder;
 @property (nonatomic, strong) AEAudioFilePlayer *player;
 
+@property (nonatomic, strong) AEAudioUnitFilter *reverb;
+@property (nonatomic, strong) AEAudioUnitFilter *delay;
+@property (nonatomic, strong) AEAudioUnitFilter *reverb2;
+
+
+
 
 @end
 
+@implementation PlayScreenViewController
 
-
-
-@implementation ViewController
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
+    // Do any additional setup after loading the view.
     self.audioController = [[AEAudioController alloc] initWithAudioDescription:[AEAudioController nonInterleaved16BitStereoAudioDescription] inputEnabled:YES];
     _audioController.preferredBufferDuration = 0.005;
     _audioController.useMeasurementMode = YES;
@@ -43,8 +41,8 @@
     
     // Create the first loop player
     _loop1 = [AEAudioFilePlayer audioFilePlayerWithURL:[[NSBundle mainBundle] URLForResource:@"Gangster hip hop" withExtension:@"mp3"]
-                                           audioController:_audioController
-                                                     error:NULL];
+                                       audioController:_audioController
+                                                 error:NULL];
     _loop1.volume = 1.0;
     _loop1.channelIsMuted = YES;
     _loop1.loop = YES;
@@ -53,51 +51,16 @@
         NSLog(@"loop is null");
     }
     
-   [_audioController addChannels:@[self.loop1]];
-    
-    
-
+    [_audioController addChannels:@[self.loop1]];
 }
 
-
-- (IBAction)RecordButton:(UIButton *)sender {
-    
-    if ( _recorder ) {
-        [_recorder finishRecording];
-        [_audioController removeOutputReceiver:_recorder];
-        [_audioController removeInputReceiver:_recorder];
-        self.recorder = nil;
-        NSLog(@"recorder ended");
-        //_recordButton.selected = NO;
-    } else {
-        self.recorder = [[AERecorder alloc] initWithAudioController:_audioController];
-        NSArray *documentsFolders = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString *path = [documentsFolders[0] stringByAppendingPathComponent:@"Recording.aiff"];
-        NSError *error = nil;
-        if ( ![_recorder beginRecordingToFileAtPath:path fileType:kAudioFileAIFFType error:&error] ) {
-            [[[UIAlertView alloc] initWithTitle:@"Error"
-                                        message:[NSString stringWithFormat:@"Couldn't start recording: %@", [error localizedDescription]]
-                                       delegate:nil
-                              cancelButtonTitle:nil
-                              otherButtonTitles:@"OK", nil] show];
-            self.recorder = nil;
-            return;
-        }
-        
-       // _recordButton.selected = YES;
-        
-        [_audioController addOutputReceiver:_recorder];
-        [_audioController addInputReceiver:_recorder];
-         NSLog(@"recorder created");
-    }
-}
 
 - (IBAction)PlayButton:(UIButton *)sender {
     
     if ( _player ) {
         [_audioController removeChannels:@[_player]];
         self.player = nil;
-//        _playButton.selected = NO;
+        //        _playButton.selected = NO;
     } else {
         NSArray *documentsFolders = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *path = [documentsFolders[0] stringByAppendingPathComponent:@"Recording.aiff"];
@@ -123,21 +86,41 @@
         NSLog(@"Played for 5 seconds %f",_loop1.currentTime);
         
         _player.removeUponFinish = YES;
-        __weak ViewController *weakSelf = self;
+      //  __weak UIViewController *weakSelf = self;
         _player.completionBlock = ^{
-//            ViewController *strongSelf = weakSelf;
-//            strongSelf->_playButton.selected = NO;
-            weakSelf.player = nil;
+            //            ViewController *strongSelf = weakSelf;
+            //            strongSelf->_playButton.selected = NO;
+     
         };
         [_audioController addChannels:@[_player]];
+        _loop1.channelIsMuted = NO;
         
-       
+        
         
         [_loop1 setCurrentTime:5.00];
         
     }
-
+    
 }
+
+
+//============================DELAY BLOCK==========================
+- (void)delayVolueChanged:(UISlider*)sender {
+    
+    if(!_delay){
+        self.delay = [[AEAudioUnitFilter alloc] initWithComponentDescription:AEAudioComponentDescriptionMake(kAudioUnitManufacturer_Apple, kAudioUnitType_Effect,kAudioUnitSubType_Delay) audioController:_audioController error:NULL] ;
+        [_audioController addFilter:_delay];
+    }
+    checkResult( AudioUnitSetParameter(_delay.audioUnit,kDelayParam_WetDryMix,
+                                       0, kAudioUnitScope_Global,
+                                       sender.value, 0),
+                "AudioUnitSetProperty(kDelayParam_WetDryMix)");
+    
+    NSLog(@"delay stufff called");
+    
+}
+//=================================================================
+
 
 
 
@@ -146,5 +129,16 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
